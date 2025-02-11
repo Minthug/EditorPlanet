c64563ff2de641f5a4101d7e5764e24a9f9972ec
minthug <jkisimmortal4@gmail.com>
2025-02-11 15:57:07 +0900

    Add DirectMessage entity with essential fields and methods

--- SettingServer/src/main/java/setting/SettingServer/entity/DirectMessage.java
+++ SettingServer/src/main/java/setting/SettingServer/entity/DirectMessage.java
@@ -1,4 +1,43 @@
 package setting.SettingServer.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DirectMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recevier_id", nullable = false)
+    private Member receiver;
+
+    private String content;
+
+    private boolean isRead;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime sendAt;
+
+    public void markAsRead() {
+        this.isRead = true;
+    }
+
 }
