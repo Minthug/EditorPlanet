5585b725b7c9a084695ee22a13098d457430f5a9
minthug <jkisimmortal4@gmail.com>
2025-03-18 16:18:24 +0900

    Add validation constraints to chat request DTOs

--- SettingServer/src/main/java/setting/SettingServer/dto/chat/CreateGroupChatRoomRequest.java
+++ SettingServer/src/main/java/setting/SettingServer/dto/chat/CreateGroupChatRoomRequest.java
@@ -1,4 +1,13 @@
 package setting.SettingServer.dto.chat;
 
-public record CreateGroupChatRoomRequest() {
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+/**
+ * 채팅방 생성 요청 DTO - GROUP CHAT
+ * @param name
+ * @param memberIds
+ */
+public record CreateGroupChatRoomRequest(String name, @NotEmpty(message = "최소 1명 이상의 멤버가 필요합니다")List<String> memberIds) {
 }
--- SettingServer/src/main/java/setting/SettingServer/dto/chat/UpdateChatRoomNameRequest.java
+++ SettingServer/src/main/java/setting/SettingServer/dto/chat/UpdateChatRoomNameRequest.java
@@ -1,4 +1,10 @@
 package setting.SettingServer.dto.chat;
 
-public record UpdateChatRoomNameRequest() {
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * 채팅방 이름 변경 요청 DTO
+ * @param name
+ */
+public record UpdateChatRoomNameRequest(@NotBlank(message = "채팅방 이름은 필수 입니다") String name) {
 }
