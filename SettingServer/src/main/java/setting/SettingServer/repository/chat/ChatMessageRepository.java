145c3e7ce199bc5de1aef50954f77c266316a48c
minthug <jkisimmortal4@gmail.com>
2025-03-19 17:46:29 +0900

    Add ChatMessageRepository to manage chat message data

---
+++ SettingServer/src/main/java/setting/SettingServer/repository/chat/ChatMessageRepository.java
@@ -0,0 +1,22 @@
+package setting.SettingServer.repository.chat;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import setting.SettingServer.entity.chat.ChatMessage;
+import setting.SettingServer.entity.chat.ChatRoom;
+import setting.SettingServer.entity.chat.MessageType;
+
+import java.util.Optional;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    Page<ChatMessage> findByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom, Pageable pageable);
+
+    Optional<ChatMessage> findTopByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom);
+
+    long countByChatRoomAndMessageTypeNot(ChatRoom chatRoom, MessageType messageType);
+
+    long countByChatRoomAndIdGreaterThanAndMessageTypeNot(ChatRoom chatRoom, Long messageId, MessageType messageType);
+
+}
