f571cbe69d7b556cb9858c82a9f046ffe7733d7d
minthug <jkisimmortal4@gmail.com>
2025-03-24 16:05:11 +0900

    Add group chat room creation functionality

--- SettingServer/src/main/java/setting/SettingServer/controller/ChatRoomController.java
+++ SettingServer/src/main/java/setting/SettingServer/controller/ChatRoomController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import setting.SettingServer.dto.chat.ChatRoomDto;
 import setting.SettingServer.dto.chat.CreateDirectChatRoomRequest;
+import setting.SettingServer.dto.chat.CreateGroupChatRoomRequest;
 import setting.SettingServer.service.chat.ChatRoomService;
 
 @RestController
@@ -19,12 +20,31 @@ public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
 
+    /**
+     * 1:1 채팅방 생성
+     * @param request
+     * @return
+     */
     @PostMapping("/direct")
     public ResponseEntity<ChatRoomDto> createDirectChatRoom(@RequestBody @Valid CreateDirectChatRoomRequest request) {
         log.info("1:1 채팅방 생성 요청: {}", request);
         ChatRoomDto chatRoom = chatRoomService.createDirectChatRoom(request.userId(), request.targetUserId());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(chatRoom);
+    }
+
+    /**
+     * 그룹 채팅방 생성
+     * @param request
+     * @return
+     */
+    @PostMapping("/group")
+    public ResponseEntity<ChatRoomDto> createGroupChatRoom(@RequestBody @Valid CreateGroupChatRoomRequest request) {
+        log.info("그룹 채팅방 생성 요청: {}", request);
+
+        ChatRoomDto chatRoomDto = chatRoomService.createGroupChatRoom(request.name(), request.creatorId(), request.memberIds());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(chatRoomDto);
     }
 
 
--- SettingServer/src/main/java/setting/SettingServer/dto/chat/CreateGroupChatRoomRequest.java
+++ SettingServer/src/main/java/setting/SettingServer/dto/chat/CreateGroupChatRoomRequest.java
@@ -1,6 +1,7 @@
 package setting.SettingServer.dto.chat;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
@@ -9,5 +10,5 @@ import java.util.List;
  * @param name
  * @param memberIds
  */
-public record CreateGroupChatRoomRequest(String name, @NotEmpty(message = "최소 1명 이상의 멤버가 필요합니다")List<String> memberIds) {
+public record CreateGroupChatRoomRequest(String name, @NotNull(message = "생성자 ID는 필수 입니다") Long creatorId, @NotEmpty(message = "최소 1명 이상의 멤버가 필요합니다")List<Long> memberIds) {
 }
