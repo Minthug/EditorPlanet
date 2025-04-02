6303fdaa64ddd157191eb2888ca1ea3e78961638
minthug <jkisimmortal4@gmail.com>
2025-04-02 17:19:08 +0900

    Add Oauth2.0 function Kakao and Naver

---
+++ frontend/editor/src/app/api/auth/google-profile/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+    const res = await fetch(process.env.JAVA_BACKEND_URL + `/v1/auth/profile`, {
+        method: 'GET',
+        headers: {
+            authorization: req.headers.get('authorization') || ''
+        },
+        cache: 'no-store'
+    })
+
+    if (!res.ok) {
+        const { message, error, statusCode } = await res.json()
+        return NextResponse.json({ message, error }, { status: statusCode })
+    }
+
+    const data = await res.json()
+    return NextResponse.json({ message: 'success', data }, { status: 200 })
+}   
